fbc0169818e27fba743a907ab6600951d4ae9892
oxanative <coffeemilkcrazy@gmail.com>
2017-01-12 09:47:47 +0900

    first commit
    first commit

---
+++ Anken/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace Anken
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.txtFilter = new System.Windows.Forms.TextBox();
+            this.btnFilter = new System.Windows.Forms.Button();
+            this.btnRelease = new System.Windows.Forms.Button();
+            this.bsAnken = new System.Windows.Forms.BindingSource(this.components);
+            this.dsAnken = new Anken.AnkenDataSet();
+            this.Selected = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.iDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.unitDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.stationDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.chkSelected = new System.Windows.Forms.CheckBox();
+            this.btnOut = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bsAnken)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dsAnken)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowDrop = true;
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Selected,
+            this.iDDataGridViewTextBoxColumn,
+            this.unitDataGridViewTextBoxColumn,
+            this.stationDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.bsAnken;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(1023, 232);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellValidated += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValidated);
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            this.dataGridView1.DragDrop += new System.Windows.Forms.DragEventHandler(this.dataGridView1_DragDrop);
+            this.dataGridView1.DragEnter += new System.Windows.Forms.DragEventHandler(this.dataGridView1_DragEnter);
+            // 
+            // textBox1
+            // 
+            this.textBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.bsAnken, "Memo", true));
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(1023, 512);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.WordWrap = false;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(12, 38);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.dataGridView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.textBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(1023, 748);
+            this.splitContainer1.SplitterDistance = 232;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // txtFilter
+            // 
+            this.txtFilter.Location = new System.Drawing.Point(12, 12);
+            this.txtFilter.Name = "txtFilter";
+            this.txtFilter.Size = new System.Drawing.Size(367, 19);
+            this.txtFilter.TabIndex = 0;
+            this.txtFilter.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtFilter_KeyDown);
+            // 
+            // btnFilter
+            // 
+            this.btnFilter.Location = new System.Drawing.Point(460, 9);
+            this.btnFilter.Name = "btnFilter";
+            this.btnFilter.Size = new System.Drawing.Size(75, 24);
+            this.btnFilter.TabIndex = 1;
+            this.btnFilter.Text = "フィルタ";
+            this.btnFilter.UseVisualStyleBackColor = true;
+            this.btnFilter.Click += new System.EventHandler(this.btnFilter_Click);
+            // 
+            // btnRelease
+            // 
+            this.btnRelease.Location = new System.Drawing.Point(541, 10);
+            this.btnRelease.Name = "btnRelease";
+            this.btnRelease.Size = new System.Drawing.Size(75, 24);
+            this.btnRelease.TabIndex = 2;
+            this.btnRelease.Text = "解除";
+            this.btnRelease.UseVisualStyleBackColor = true;
+            this.btnRelease.Click += new System.EventHandler(this.btnRelease_Click);
+            // 
+            // bsAnken
+            // 
+            this.bsAnken.DataMember = "AnkenTbl";
+            this.bsAnken.DataSource = this.dsAnken;
+            // 
+            // dsAnken
+            // 
+            this.dsAnken.DataSetName = "AnkenDataSet";
+            this.dsAnken.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // Selected
+            // 
+            this.Selected.DataPropertyName = "Selected";
+            this.Selected.HeaderText = "Selected";
+            this.Selected.Name = "Selected";
+            this.Selected.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Selected.Width = 30;
+            // 
+            // iDDataGridViewTextBoxColumn
+            // 
+            this.iDDataGridViewTextBoxColumn.DataPropertyName = "ID";
+            this.iDDataGridViewTextBoxColumn.HeaderText = "ID";
+            this.iDDataGridViewTextBoxColumn.Name = "iDDataGridViewTextBoxColumn";
+            this.iDDataGridViewTextBoxColumn.ReadOnly = true;
+            this.iDDataGridViewTextBoxColumn.Width = 300;
+            // 
+            // unitDataGridViewTextBoxColumn
+            // 
+            this.unitDataGridViewTextBoxColumn.DataPropertyName = "Unit";
+            this.unitDataGridViewTextBoxColumn.HeaderText = "Unit";
+            this.unitDataGridViewTextBoxColumn.Name = "unitDataGridViewTextBoxColumn";
+            this.unitDataGridViewTextBoxColumn.ReadOnly = true;
+            this.unitDataGridViewTextBoxColumn.Width = 200;
+            // 
+            // stationDataGridViewTextBoxColumn
+            // 
+            this.stationDataGridViewTextBoxColumn.DataPropertyName = "Station";
+            this.stationDataGridViewTextBoxColumn.HeaderText = "Station";
+            this.stationDataGridViewTextBoxColumn.Name = "stationDataGridViewTextBoxColumn";
+            this.stationDataGridViewTextBoxColumn.ReadOnly = true;
+            this.stationDataGridViewTextBoxColumn.Width = 200;
+            // 
+            // chkSelected
+            // 
+            this.chkSelected.AutoSize = true;
+            this.chkSelected.Location = new System.Drawing.Point(385, 15);
+            this.chkSelected.Name = "chkSelected";
+            this.chkSelected.Size = new System.Drawing.Size(69, 16);
+            this.chkSelected.TabIndex = 3;
+            this.chkSelected.Text = "選択のみ";
+            this.chkSelected.UseVisualStyleBackColor = true;
+            // 
+            // btnOut
+            // 
+            this.btnOut.Location = new System.Drawing.Point(960, 7);
+            this.btnOut.Name = "btnOut";
+            this.btnOut.Size = new System.Drawing.Size(75, 24);
+            this.btnOut.TabIndex = 4;
+            this.btnOut.Text = "出力";
+            this.btnOut.UseVisualStyleBackColor = true;
+            this.btnOut.Click += new System.EventHandler(this.btnOut_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1047, 798);
+            this.Controls.Add(this.btnOut);
+            this.Controls.Add(this.chkSelected);
+            this.Controls.Add(this.btnRelease);
+            this.Controls.Add(this.btnFilter);
+            this.Controls.Add(this.txtFilter);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "PE Viewer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.bsAnken)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dsAnken)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AnkenDataSet dsAnken;
+        private System.Windows.Forms.BindingSource bsAnken;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox txtFilter;
+        private System.Windows.Forms.Button btnFilter;
+        private System.Windows.Forms.Button btnRelease;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Selected;
+        private System.Windows.Forms.DataGridViewTextBoxColumn iDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn unitDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn stationDataGridViewTextBoxColumn;
+        private System.Windows.Forms.CheckBox chkSelected;
+        private System.Windows.Forms.Button btnOut;
+    }
+}
+
---
+++ Anken/Form1.cs
@@ -0,0 +1,314 @@
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Anken
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //デフォルトのセルスタイルの設定
+            this.defaultCellStyle = new DataGridViewCellStyle();
+            //現在のセルのセルスタイルの設定
+            this.mouseCellStyle = new DataGridViewCellStyle();
+            this.mouseCellStyle.BackColor = Color.LightBlue;
+            this.mouseCellStyle.SelectionBackColor = Color.Blue;
+
+
+            if (File.Exists(dsfile))
+            {
+                dsAnken.ReadXml(dsfile);
+                RefreshRowColor();
+            }
+
+
+
+        }
+
+        private void dataGridView1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        }
+
+        private void dataGridView1_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                dsAnken.Clear();
+                foreach (string fileName in (string[])e.Data.GetData(DataFormats.FileDrop))
+                {
+                    readFile(fileName);
+                }
+
+                RefreshRowColor();
+            }
+        }
+
+        private const string dsfile = @"anken.xml";
+
+        private void readFile(string filename)
+        {
+            string line = string.Empty;
+
+            //string all = ReadToEnd(filename);
+
+            using (StreamReader sr = new StreamReader(filename, Encoding.GetEncoding("Shift_JIS")))
+            {
+                AnkenDataSet.AnkenTblRow r = null;
+                StringBuilder sb = new StringBuilder();
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (HasSplitter(line))
+                    {
+                        if (r != null)
+                        {
+                            r.Memo = sb.ToString();
+                        }
+                        continue;
+                    }
+
+                    if (line.Contains("◆担当："))
+                    {
+                        r = dsAnken.AnkenTbl.NewAnkenTblRow();
+                        dsAnken.AnkenTbl.AddAnkenTblRow(r);
+                        r.ID = line;
+                        sb.Clear();
+                    }
+                    if (HasStation(line))
+                    {
+                        r.Station = line;
+                    }
+                    if (HasTanka(line))
+                    {
+                        r.Unit = line;
+                    }
+                    if (!IsIgnore(line))
+                    {
+                        sb.Append(line).Append("\r\n");
+                    }
+                }
+                if (r != null)
+                {
+                    r.Memo = sb.ToString();
+                }
+            }
+
+            dsAnken.WriteXml(dsfile);
+        }
+
+        static string[] ignore = {
+                                      "案件情報（",
+                                      "ページ(",
+                                  };
+        private bool IsIgnore(string s)
+        {
+            foreach (var key in ignore)
+            {
+                if (s.Contains(key))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        static string[] station = {
+                                      "【勤務地】",
+                                      "最寄り駅：",
+                                      "場所",
+                                  };
+
+        private bool HasStation(string s)
+        {
+            foreach (var st in station)
+            {
+                if (s.Contains(st))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        static string[] unit = {
+                                      "万円",
+                                      //"単価",
+                                  };
+
+        private bool HasTanka(string s)
+        {
+            foreach (var st in unit)
+            {
+                if (s.Contains(st))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private bool HasSplitter(string s)
+        {
+            return s.Contains("==============================================================================================");
+        }
+
+        private void btnFilter_Click(object sender, EventArgs e)
+        {
+
+            FilterIt();
+        }
+        private string ReadToEnd(string input)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            using (PdfReader reader = new PdfReader(input))
+            {
+                ITextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
+
+                //Page
+                for (int i = 1; i <= reader.NumberOfPages; i++)
+                {
+                    string curPage = PdfTextExtractor.GetTextFromPage(reader, i, strategy);
+                    builder.Append(curPage);
+
+                    /* もし一行ずつ読みたいならこちらを挿入し、直上のbuilder.Append(curPage);を削除
+                    string[] lineSet = curPage.Split('\n');
+                    foreach (string line in lineSet) {
+                        builder.Append(line);
+                    }
+                     */
+                }
+                reader.Close();
+            }
+
+            return builder.ToString();
+        }
+        private string CreateFilter(string col, string val)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(col);
+            sb.Append(" LIKE '%");
+            sb.Append(val);
+            sb.Append("%' ");
+            return sb.ToString();
+        }
+
+        private void txtFilter_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                FilterIt();
+            }
+        }
+
+        private void FilterIt()
+        {
+            string s = txtFilter.Text;
+            StringBuilder sb = new StringBuilder();
+            if (s.Trim().Length != 0)
+            {
+                sb.Append(CreateFilter("Memo", s));
+            }
+
+
+            if (chkSelected.Checked)
+            {
+                if (sb.Length != 0)
+                {
+                    sb.Append(" AND ");
+                }
+                sb.Append("Selected = True");
+            }
+
+            bsAnken.Filter = sb.ToString();
+            RefreshRowColor();
+
+        }
+
+        private void btnRelease_Click(object sender, EventArgs e)
+        {
+            txtFilter.Text = string.Empty;
+            FilterIt();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            dsAnken.WriteXml(dsfile);
+        }
+        //デフォルトのセルスタイル
+        private DataGridViewCellStyle defaultCellStyle;
+        //マウスポインタの下にあるセルのセルスタイル
+        private DataGridViewCellStyle mouseCellStyle;
+
+        private void RefreshRowColor()
+        {
+            foreach (DataGridViewRow r in dataGridView1.Rows)
+            {
+                DataRowView drv = (DataRowView)r.DataBoundItem;
+                AnkenDataSet.AnkenTblRow dr = (AnkenDataSet.AnkenTblRow)drv.Row;
+                if (dr.Selected)
+                {
+                    ChangeStyle(r, mouseCellStyle);
+                }
+                else
+                {
+                    ChangeStyle(r, defaultCellStyle);
+                }
+            }
+        }
+
+        private void ChangeStyle(DataGridViewRow r, DataGridViewCellStyle style)
+        {
+            foreach (DataGridViewCell c in r.Cells)
+            {
+                c.Style = style;
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            RefreshRowColor();
+        }
+
+        private void dataGridView1_CellValidated(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            RefreshRowColor();
+
+        }
+
+        private void btnOut_Click(object sender, EventArgs e)
+        {
+            var list = (from d in dsAnken.AnkenTbl
+                        where d.Selected == true
+                        select d).ToArray();
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var r in list)
+            {
+                sb.Append("==============================================================================================\n");
+                sb.Append(r.Memo);
+            }
+            Clipboard.SetText(sb.ToString());
+        }
+    }
+}
